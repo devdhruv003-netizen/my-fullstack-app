3cd1508fe909f723dc3298c878b5c7959521a120
devDhruv <devdhruv03@gmail.com>
2026-01-16 18:10:42 +0530

    middlware Changes

--- middleware.js
+++ middleware.js
@@ -1,18 +1,10 @@
-import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher(['/about(.*)']);
-
-export default clerkMiddleware((auth,req)=>{
-    if(isProtectedRoute(req)) {
-        auth.protect();
-    }
+export default authMiddleware({
+  // Ye routes bina login ke khulenge
+  publicRoutes: ["/", "/about"] 
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
